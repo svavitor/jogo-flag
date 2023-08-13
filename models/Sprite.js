716114061c7b5e77918b6caa3cbc8c4f06fa81ef
Vitor Senna <vitor.senna@imd.ufrn.br>
2023-08-13 14:17:48 -0300

    classe elemento e sprite criadas

---
+++ models/Sprite.js
@@ -0,0 +1,16 @@
+class Sprite {
+    constructor(
+        atual,
+        orientacao,
+        esquerda,
+        direita,
+        pulandoEsquerda,
+        pulandoDireita){
+        this.atual = atual;
+        this.orientacao = orientacao;
+        this.esquerda = esquerda;
+        this.direita = direita;
+        this.pulandoEsquerda = pulandoEsquerda;
+        this.pulandoDireita = pulandoDireita;
+    }
+}
